11a99aa317f20de3a6ebd3f7c224ec63e1817ee2
ubuntu <roma.mark.222@gmail.com>
2025-09-03 16:17:09 +0000

    git_1.6

--- main.py
+++ main.py
@@ -2,4 +2,3 @@ a = 8;
 b = 2;
 c = a + b;
 print(f"Hello from Docker {c}")
-~                                                                                                                          
